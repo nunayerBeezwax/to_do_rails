9d625ce3b848bda5a643c573a7740a51e9bfd62d
Harland Hauger & Kimberlee Redman <student@epicodus.com>
2014-03-31 12:33:19 -0700

    To do list is ugly but working. needs done demarcation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ToDoRails::Application.routes.draw do
   match('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks', {:via => :post, :to => 'tasks#create'})
+  match('tasks/new', {:via => :get, :to => 'tasks#new'})
   match('tasks/:id', {:via => :get, :to => 'tasks#show'})
+  match('tasks/:id/edit', {:via => :get, :to => 'tasks#edit'})
+  match('tasks/:id', {:via => [:patch, :put], :to => 'tasks#update'})
 end
